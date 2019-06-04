5a67e29e5d4f14333781946a20908bfa5d25f099
Justin Hutsell <hutselljustin@gmail.com>
2019-06-04 00:18:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
 
+def sort_array_asc(arr)
+  arr.sort
+end
+
+def sort_array_desc(arr)
+  arr.sort { |a,b| b <=> a }
+end
+
+def sort_array_char_count(arr)
+  arr.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(arr)
+  arr[3], arr[4] = arr[4], arr[3]
+end
